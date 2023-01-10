3df5e2101f9803ad8db37807cb18cde059356355
shreyas <pshreyas1224@gmail.com>
2023-01-10 11:38:44 +0530

    added database and dynamic list item. next task is to delete items from respected list

--- date.js
+++
@@ -1,9 +0,0 @@
-
-module.exports= getDate;
-
-function getDate(){
-    const options = { weekday: 'long', month: 'long', day: 'numeric' };
-    const today = new Date();
-    const day = today.toLocaleDateString("en-US", options);
-    return day;
-}
--- index.js
+++ index.js
@@ -1,51 +1,137 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-
+const mongoose = require("mongoose");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 
-const tasks = [];
-const workTasks = [];
 
+// connecting to database and creating schema.
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://127.0.0.1:27017/todoList", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(() => {
+    console.log("successfully connected to the database");
+}).catch((error) => {
+    console.log(error);
+});
+
+const itemSchema = new mongoose.Schema({
+    name: String
+});
+
+
+const listSchema = new mongoose.Schema({
+    name:String,
+    items : [itemSchema]
+});
+
+
+//model for listSchema
+const ListModel = new mongoose.model("ListModel" , listSchema);
+const ItemCollection = new mongoose.model("ItemCollection", itemSchema);
+var tasks;
 // get for home
 app.get("/", (req, res) => {
-    day = date();
-    res.render("list" , {day: day,
-        newTask : tasks    
+
+    ItemCollection.find({}, (error, data) => {
+        if (error) {
+            console.log(error);
+            res.render("list", {
+                day: "Today",
+                newTask: []
+            });
+        }
+        else {
+            res.render("list", {
+                day: "Today",
+                newTask: data
+            })
+        }
     });
 });
 
 
-// get for work
-app.get("/workday" , (req , res)=>{
-    res.render("list" , {day : "HustleDay" , 
-        newTask : workTasks});
+
+// delete from list.
+app.post("/delete", (req, res)=>{
+    var checked_id = req.body.checkbox;
+    console.log(checked_id);
+    ItemCollection.findByIdAndRemove(checked_id , (error)=>{
+        if(error){
+            console.log(error);
+        }
+    });
+    res.redirect("/");
+    
+});
+
+// dynamic url
+app.get("/:urlParameter" , (req , res)=>{
+    const listType = req.params.urlParameter;
+    
+    ListModel.findOne({name : listType} , (error ,result)=>{
+        if(!error){
+            if(!result){
+                const listDoc = new ListModel({
+                    name : listType,
+                    items : []
+                });
+                listDoc.save();
+                res.redirect("/" + listType);
+            }
+            else{
+                // show existing list.
+                res.render("list" , {
+                    day : result.name,
+                    newTask : result.items
+                } );
+            }
+        }
+        else{
+            console.log(error);
+            res.redirect("/");
+        }
+    });
+    
 });
 
 
 // get for about
-app.get("/about" , (req , res)=>{
-    res.render("about" , {});
+app.get("/about", (req, res) => {
+    res.render("about", {});
 });
 
 
 //post for all
-app.post("/" , (req , res)=>{
+app.post("/", (req, res) => {
     const task = req.body.newItem;
-    const type = req.body.list;
+    const listName = req.body.list;
 
-    if(type == "HustleDay"){
-        workTasks.push(task);
-        res.redirect("/workday");
+    const documentItem = new ItemCollection({
+        name: task
+    });
+
+    if(listName === "today"){
+       documentItem.save();
+       res.redirect("/"); 
     }else{
-        tasks.push(task);
-        res.redirect("/");
+        ListModel.findOne({ name : listName } , (error , result)=>{
+            if(!error){
+                result.items.push(documentItem);
+                result.save();
+                res.redirect("/" + listName);
+            }else{
+                console.log(error);
+                res.redirect("/");
+            }
+        });
     }
+
 
 })
 
